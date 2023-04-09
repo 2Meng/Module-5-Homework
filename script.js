e5ab7e23dd4e0e59e26c22820fabcae209253a97
2Meng <tyan1401@gmail.com>
2023-04-09 18:07:57 -0500

    clock and time slot class add

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
+if(localStorage.getItem('dayItem')){
+  scheduleItems = JSON.parse(localStorage.getItem('dayItem')) 
+};
+
+var scheduleItems = [];
+
 $(function () {
+  //clock
+  setInterval(function(){
+    $('#clock').text(dayjs().format('MMM D, YYYY [at] hh:mm:ss A'))
+  }, 1000);
+
+  //event listener to save schedule item
+  $('.btn').click(function(){
+      
+
+  });
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -9,15 +29,26 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
+  //apply 'past'-'present'-'future' class to schedule time slot if 'todays' hour equals if statement
+  var currentHour = dayjs().hour();
+  for(var i = 6; i <= 18; i++){
+    var hourId = 'hour-' + (i);
+    var hourElement = document.getElementById(hourId);
+    
+    if(hourElement){
+      if (i === currentHour) {
+        document.getElementById(hourId).classList.add('present')
+      } 
+      if (i < currentHour){
+        document.getElementById(hourId).classList.add('past')
+      }
+      if(i > currentHour){
+        document.getElementById(hourId).classList.add('future')
+      }
+    }
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
