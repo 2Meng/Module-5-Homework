5d98e635f9256d657336ecd765e8163d32737abe
2Meng <tyan1401@gmail.com>
2023-04-10 16:34:45 -0500

    final js

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+ //clock
+setInterval(function(){
+  $('#clock').text(dayjs().format('MMM D, YYYY [at] h:mm A'))
+}, 1000);
 
-
-
-if(localStorage.getItem('dayItem')){
-  scheduleItems = JSON.parse(localStorage.getItem('dayItem')) 
-};
-
-var scheduleItems = [];
-
-$(function () {
-  //clock
-  setInterval(function(){
-    $('#clock').text(dayjs().format('MMM D, YYYY [at] hh:mm:ss A'))
-  }, 1000);
+$(document).ready(function () {
 
   //event listener to save schedule item
-  $('.btn').click(function(){
-      
-
-  });
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  $(".saveBtn").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  })
 
   //apply 'past'-'present'-'future' class to schedule time slot if 'todays' hour equals if statement
-  var currentHour = dayjs().hour();
+  function timeTracker(){
+    var currentHour = dayjs().hour();
   for(var i = 6; i <= 18; i++){
     var hourId = 'hour-' + (i);
     var hourElement = document.getElementById(hourId);
@@ -48,7 +33,21 @@ $(function () {
       }
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+  }
+
+  $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+  $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+  $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+
+  timeTracker()
 });
